43c17e5c1aa22ebe6e1ca7ccb1044b28ee356461
Al White <alcwhite13@gmail.com>
2020-04-20 13:35:31 -0400

    Add scale component

---
+++ lib/scale_generator_web/live/scale_component.ex
@@ -0,0 +1,19 @@
+defmodule ScaleGeneratorWeb.ScaleComponent do
+  use Phoenix.LiveComponent
+
+  def render(assigns) do
+    directions = %{"ascending" => :asc, "descending" => :desc}
+    ~L"""
+    <p class="lead"><%= @direction %></p>
+    <h2>
+        <%= Enum.map(Enum.with_index(ScaleGeneratorWeb.FormsView.scale(@tonic, @scale, Map.get(directions, @direction))), fn {note, i} -> %>
+            <span id="<%= i %>-<%= to_string(Map.get(directions, @direction)) %>" class="note"><%= note %></span>
+        <% end) %>
+    </h2>
+    """
+  end
+end
+
+
+
+
