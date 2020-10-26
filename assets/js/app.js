ef2f6536472f900477e028c83a59055c681a60c5
Al White <alcwhite13@gmail.com>
2020-10-26 13:19:32 -0400

    Move hooks to own file

--- assets/js/app.js
+++ assets/js/app.js
@@ -18,58 +18,9 @@ import "phoenix_html"
 import {Socket} from "phoenix"
 import LiveSocket from "phoenix_live_view"
 
+import Hooks from "./hooks"
+
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content");
-
-let Hooks = {}
-
-Hooks.PlayScale = {
-    timeouts: [],
-    scale: "",
-    action: "",
-    allNotes: [],
-    mounted() {
-        this.scale = this.el.getAttribute("data-recording");
-        this.action = this.el.getAttribute("data-action");
-        this.el.addEventListener("click", () => this.playScale());
-    },
-    updated() {
-        this.scale = this.el.getAttribute("data-recording");
-        if (this.action == "Stop") {
-            this.playScale()
-        }
-        this.action = this.el.getAttribute("data-action");
-        this.el.addEventListener("click", () => this.playScale());
-    },
-    playScale() {
-        if (this.action === "Play") {
-            this.action = "None";
-            this.allNotes = Array.from(this.el.parentElement.getElementsByClassName("note"));
-            this.pushEvent("play", {});
-            let notes = this.scale.split(" ").map(note => new Audio(`/sounds/${note}.mp3`));
-            this.timeouts = notes.map((note, i) => setTimeout(() => {
-                this.allNotes.forEach(note => {
-                    note.className = "note"; 
-                 });
-                 this.allNotes[i].className = "note glow";
-                note.play();
-            }, 750 * i + 1200));
-            this.timeouts.push(setTimeout(() => {
-                    this.allNotes.forEach(note => {
-                       note.className = "note"; 
-                    });
-                    this.pushEvent("stop", {});
-                }, notes.length * 750 + 1200));
-        }
-        if (this.action === "Stop") {
-            this.action = "None";
-            this.timeouts.map(t => clearTimeout(t));
-            this.allNotes.forEach(note => {
-                note.className = "note"; 
-             });
-            this.pushEvent("stop", {});
-        }
-    }
-}
 
 let liveSocket = new LiveSocket("/live", Socket, {hooks: Hooks, params: {_csrf_token: csrfToken}})
 liveSocket.connect()
