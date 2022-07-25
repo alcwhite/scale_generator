558b197d66f03a35ef938247a4805e7daecad300
Al White <al@gaslight.co>
2022-07-25 16:00:33 -0400

    WIP

--- assets/js/hooks.js
+++ assets/js/hooks.js
@@ -1,3 +1,41 @@
 let Hooks = {}
 
+Hooks.PlayScale = {
+    timeouts: [],
+    scale: "",
+    action: "Play",
+    allNotes: [],
+    updated() {
+        this.action = this.el.getAttribute("data-action");
+        this.el.innerText = this.action;
+        this.scale = this.el.getAttribute("data-recording");
+        if (this.action == "Stop") {
+            this.playScale();
+        } else {
+            this.stopPlayer();
+        }
+    },
+    stopPlayer() {
+        this.timeouts.map(t => clearTimeout(t));
+        this.allNotes.forEach(note => {
+            note.className = "note"; 
+        });
+    },
+    playScale() {
+        if (this.action === "Stop") {
+            this.allNotes = Array.from(this.el.parentElement.getElementsByClassName("note"));
+            let notes = this.scale.split(" ").map(note => new Audio(`/sounds/${note}.mp3`));
+            this.timeouts = notes.map((note, i) => setTimeout(() => {
+                if (i > 0) { this.allNotes[i - 1].classList.remove("glow"); }
+                this.allNotes[i].classList.add("glow");
+                note.play();
+            }, 750 * i + 100));
+            this.timeouts.push(setTimeout(() => {
+                this.stopPlayer();
+                this.pushEvent("clickplayer", {});
+            }, notes.length * 750 + 100));
+        }
+    }
+}
+
 export default Hooks;
--- lib/scale_generator_web/live/main_live.ex
+++ lib/scale_generator_web/live/main_live.ex
@@ -5,8 +5,6 @@ defmodule ScaleGeneratorWeb.MainLive do
   alias ScaleGenerator.Scales
   alias Phoenix.PubSub
   alias ScaleGenerator.Helpers
-  alias ScaleGenerator.ScaleGeneratorLogic
-  alias ScaleGenerator.ScaleRecorder
 
   import ScaleGeneratorWeb.{FormsComponent, ScaleComponent}
 
@@ -25,7 +23,7 @@ defmodule ScaleGeneratorWeb.MainLive do
        tonic: @defaults.tonic,
        scale: @defaults.name,
        all_tonics: @all_tonics,
-       play_text: "Play",
+       player_text: "Play",
        link: "/forms",
        link_name: "Update Scales",
        recording: Helpers.record(@defaults.name, @defaults.tonic),
@@ -35,18 +33,17 @@ defmodule ScaleGeneratorWeb.MainLive do
 
   def handle_event("change", %{"scale_form" => %{"tonic" => tonic, "name" => name}}, socket) do
     {:noreply,
-     assign(socket, %{tonic: tonic, scale: name, recording: Helpers.record(name, tonic)})}
+     assign(socket, %{player_text: "Play", tonic: tonic, scale: name, recording: Helpers.record(name, tonic)})}
   end
 
-  def handle_event("stop", _event, socket) do
-    {:noreply, assign(socket, :play_text, "Play")}
-  end
-
-  def handle_event("play", _event, socket) do
-    {:noreply, assign(socket, :play_text, "Stop")}
+  def handle_event("clickplayer", _event, socket) do
+    {:noreply, assign(socket, %{player_text: toggle_text(socket.assigns.player_text)})}
   end
 
   def handle_info({_action, list}, socket) do
     {:noreply, assign(socket, :all_scales, list)}
   end
+
+  defp toggle_text("Stop"), do: "Play"
+  defp toggle_text("Play"), do: "Stop"
 end
